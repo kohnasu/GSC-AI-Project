a679b7f3bf27453773633eaba82c5da191649db3
fusawayugo <fusawa0712@gmail.com>
2025-09-13 14:45:09 +0900

    aaa

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 CAN_FINISH_MINUTES = 10
 MUST_FINISH_MINUTES = 30
+
+MINIMUM_TURNS = 20
+
+EVERYDAY_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSc-EPNJYSgdEC7woIDky-RRjLVJx-uGs9u-awbHPPiGz5fFwg/viewform?usp=header"
+PERIODIC_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSeoUPUBFaCyWaLsuQzpiJJ1UsAVdzvx39nbou1cfZO7b6eIjg/viewform?usp=header"
